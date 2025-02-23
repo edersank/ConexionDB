7f7c947e9b47a39967efbddd1748571f3cfc77ea
Eder Morales <emoralesc@eglobal.com.mx>
2025-02-23 12:36:43 -0600

    [@emoralesc] Ejemplo base de conexion

--- conexion-oracle-ejemplo.py
+++ conexion-oracle-ejemplo.py
@@ -14,35 +14,35 @@ puerto = 1525
 host = "172.29.40.64"
 
 
-DML_QUERY = """
-                SELECT * FROM (
-                    SELECT TRL_ORIGIN_IAP_NAME IAP_ORIGEN, 
-                    TRL_DEST_IAP_NAME IAP_DESTINO,
-                    TRL_CARD_ACPT_TERMINAL_IDENT Dispatcher,
-                    TO_CHAR(TRL_SYSTEM_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS.FF3' )Fecha_Hora,
-                    TRL_ID,
-                    TRL_MESSAGE_TYPE MTI,
-                    SUBSTR(Trl_additional_response_data,94,6) PROC_CODE,---PC para 4.5
-                    TRL_CARD_ACPT_IDENT_CODE AFILIACION,
-                    substr(Trl_additional_response_data,100,3) PEM,
-                    TRL_AMT_TXN IMPORTE,
-                    TRL_PAN TRACK_II, 
-                    trl_rrn,
-                    TRL_APPROVAL_CODE NUM_AUTH
-                    from RPOS_OWNER.transaction_log
-                    Order By Trl_System_Timestamp DESC
-                ) WHERE ROWNUM <= 20"""
+# Consulta
+DML_CONSULTA = """SELECT employee_id, first_name, last_name, salary 
+                FROM employees ORDER BY employee_id DESC"""
+# Insertar
+DML_INSERT = """
+           INSERT INTO hr.employees (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, HIRE_DATE, JOB_ID, SALARY, MANAGER_ID, DEPARTMENT_ID) 
+           VALUES (212, 'Angelica', 'Regaños', 'areganos@correo.com', TO_DATE('2025-02-19 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'IT_PROG', 17000, 103, 60) 
+    """
+
+# Update
+DML_UPDATE = """
+                UPDATE hr.employees SET SALARY = 9000 WHERE EMPLOYEE_ID = 230
+    """
+
+# Delete
+DML_DELETE = """
+                DELETE FROM hr.employees WHERE employee_id = 211
+    """
 
 
 with oracledb.connect(user=usuario, password=contrasenia, port=puerto, dsn="172.29.40.64/QAPNPOS") as conexion:
     with conexion.cursor() as consulta:
-        consulta.execute(DML_QUERY)
+        consulta.execute(DML_CONSULTA)
         resultado = consulta.fetchall()
 
     # for filas in resultado:
     #     print(filas)
     #     print("------------------------------------------------------------------------------------------------------\n")
 
-headers = ["IAP_ORIGEN", "IAP_DESTINO", "DISPATCHER", "TIME STAMP", "TRL_ID",
-           "MTI", "P CODE", "AFILIACION", "PEM", "IMPORTE", "TRACK II", "RRN", "NO. AUTH."]
+headers = ["Id Empleado", "Nombre", "Apellido", "Email", "Contratacio", "Id Puesto",
+           "Salario", "Id Manager", "Id Departamento"]
 print(tabulate(resultado, headers, tablefmt="rounded_grid"))
